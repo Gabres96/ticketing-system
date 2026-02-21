e8b003c66380806f81b9450ca306b2d14950f7ef
gabres96 <gabriel.rodrigues670@gmail.com>
2026-02-21 18:03:38 -0300

    resolve 500 error on /v3/api-docs by downgrading to Spring Boot 3.4.3 and updating SpringDoc to 2.8.5

--- src/main/java/com/gabriel/ticketing/config/SecurityConfig.java
+++ src/main/java/com/gabriel/ticketing/config/SecurityConfig.java
@@ -17,9 +17,12 @@ public class SecurityConfig {
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(
-                                "/swagger-ui.html",
                                 "/swagger-ui/**",
-                                "/v3/api-docs/**"
+                                "/swagger-ui.html",
+                                "/v3/api-docs/**",
+                                "/v3/api-docs.yaml",
+                                "/swagger-resources/**",
+                                "/webjars/**"
                         ).permitAll()
                         .anyRequest().authenticated()
                 )
--- src/main/java/com/gabriel/ticketing/service/EventService.java
+++ src/main/java/com/gabriel/ticketing/service/EventService.java
@@ -5,6 +5,7 @@ import com.gabriel.ticketing.domain.event.EventRepository;
 import com.gabriel.ticketing.exception.BusinessException;
 import com.gabriel.ticketing.exception.EventNotFoundException;
 import jakarta.transaction.Transactional;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
@@ -14,18 +15,28 @@ import java.time.LocalDateTime;
 public class EventService {
 
     private final EventRepository eventRepository;
+    private final KafkaTemplate<String, Object> kafkaTemplate;
 
-    public EventService(EventRepository eventRepository) {
+    public EventService(EventRepository eventRepository, KafkaTemplate<String, Object> kafkaTemplate) {
         this.eventRepository = eventRepository;
+        this.kafkaTemplate = kafkaTemplate;
     }
 
     public Event createEvent(Event event) {
         if (event.getEventDate().isBefore(LocalDateTime.now())) {
             throw new BusinessException("Event date cannot be in the past");
         }
-        return eventRepository.save(event);
-    }
 
+        Event savedEvent = eventRepository.save(event);
+
+        try {
+            kafkaTemplate.send("event-created-topic", savedEvent.getId().toString(), savedEvent);
+        } catch (Exception e) {
+            System.err.println("Erro ao enviar para o Kafka: " + e.getMessage());
+        }
+
+        return savedEvent;
+    }
 
     public Event findById(Long id) {
         return eventRepository.findById(id)
